3033b21d22b8808cda66b1259d3a777bac4b6e19
dethMastery <me@suphakit.net>
2023-04-07 16:39:36 +0700

    refactor: code

--- app.ts
+++ app.ts
@@ -1,15 +1,15 @@
-// Stacking & Queueing 
+// Stacking & Queueing
 
 // Stacking
-import {stacking} from './src/stacking/stacking.controller'
+import { stacking } from "./src/stacking/stacking.controller";
 
-let array = [1, 2, 3, 4, 5, 'a', 'b']
+let array = [1, 2, 3, 4, 5, "a", "b"];
 
-console.log('stacking pop : ');
-stacking(array)
+console.log("stacking pop : ");
+stacking(array);
 
 // Queueing
-import { queuing } from './src/queueing/queue.controller';
+import { queuing } from "./src/queueing/queue.controller";
 
-console.log('queueing pop : ');
-queuing(array)
+console.log("queueing pop : ");
+queuing(array);
